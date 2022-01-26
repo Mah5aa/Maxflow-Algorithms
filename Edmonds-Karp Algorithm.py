65ce5f01cba3dac41940a7724349d840eafaca74
Mahsa <mahsakh987@yahoo.com>
2022-01-26 19:40:34 +0330

    Update Edmonds-Karp Algorithm.py
    Hey dear Li Zhang , I changed 'xrange' by 'range' and it works for me. this name should be changed in other algorithms .
    your code was useful  ,thank you for sharing .

--- Edmonds-Karp Algorithm.py
+++ Edmonds-Karp Algorithm.py
@@ -1,7 +1,7 @@
 #Edmonds-Karp Algorithm
 def max_flow(C, s, t):
         n = len(C) # C is the capacity matrix
-        F = [[0] * n for i in xrange(n)]
+        F = [[0] * n for i in range(n)]
         path = bfs(C, F, s, t)
       #  print path
         while path != None:
@@ -10,7 +10,7 @@ def max_flow(C, s, t):
                 F[u][v] += flow
                 F[v][u] -= flow
             path = bfs(C, F, s, t)
-        return sum(F[s][i] for i in xrange(n))
+        return sum(F[s][i] for i in range(n))
 
 #find path by using BFS
 def bfs(C, F, s, t):
@@ -20,7 +20,7 @@ def bfs(C, F, s, t):
             return paths[s]
         while queue: 
             u = queue.pop(0)
-            for v in xrange(len(C)):
+            for v in range(len(C)):
                     if(C[u][v]-F[u][v]>0) and v not in paths:
                         paths[v] = paths[u]+[(u,v)]
                         print paths
